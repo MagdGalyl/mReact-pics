7be1f506b0b15ab19378dcc83adf9ca843cec05f
“Magd <majdgalyl@gmail.com>
2021-12-06 02:01:39 +0200

    SearchBar Done & Request Prep Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 
-const App = () => {
+class App extends React.Component {
+  onSearchSubmit(term){
+    console.log(term)
+  }
+
+  render(){
     return (
-        <div>
-            <SearchBar />
-        </div>
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+        {/* {We can call above onSubmit anything it a PROP} */}
+      </div>
     );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,12 +7,18 @@ class SearchBar extends React.Component {
     //     console.log(event.target.value)
     // }
 
-    state = {term:'' }
+  state = {term:'' }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term)
+
+  }
 
     render() {
       return (
         <div className="ui segment">
-          <form className="ui form">
+          <form onSubmit= {this.onFormSubmit} className="ui form">
             <div className="field">
               <label>Image Search</label>
               {/* <input type="text" onChange={this.onInputChange} /> */}
